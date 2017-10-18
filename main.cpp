9bb7029f2511ea08bf89561b04c318194516b309
mramotowski <maciej.ramotowski@intel.com>
2017-10-18 09:29:07 +0200

    common: add test

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <gtest/gtest.h>
+#include <iostream>
+using namespace std;
+
+class FooTest : public ::testing::TestWithParam<const char*> {
+};
+
+string MyInfo(const ::testing::TestParamInfo<const char *>& info) {
+	return info.param;
+}
+
+TEST_P(FooTest, cos) {
+	cout << GetParam()  << "  " << endl;
+}
+
+INSTANTIATE_TEST_CASE_P(InstantiationName,
+                        FooTest,
+                        ::testing::Values("test1", "test2"));
+
+int main(int argc, char **argv) {
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS(); 
+
+}
